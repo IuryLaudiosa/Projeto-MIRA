df1df5cceefcf8e22cbece9dc8f0702a93a58c1a
Arthur Fernandes <arthurfernandes1666@gmail.com>
2024-05-26 16:07:27 -0300

    Estilização forum refeita.

--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -4,21 +4,32 @@
 
 @section('content')
 
-<form action="" method="post">
-    <div class="forum">
-      <h1><b>Nosso Forum</b></h1>
-      <h1>FORUM INTERATIVO</h1>
-      <div id="quadro">
-        <h3 id="nomeForum">Nome: <input type="text" name="nome" placeholder="   INSIRA SEU NOME AQUI" value=""></h3>
-        <input type="text" name="mensagem" id="campoForum" placeholder="PROIBIDO OFENSAS E EXPRESSOẼS RACISTAS"/>
-        <input type="submit" name="enviar" value="ENVIAR" id="botaoEnviar" />
-      </div>
-    </div>
-  </form>
-
-  <div class="botaoEnvio">
-    <a href="/forum.html">
-      <input id="envio" type="button" value="ENVIAR UMA MENSAGEM" /></a>
+<form action="" method="post" class="col-md-4">
+  <div class="mb-3">
+    <label for="nome" class="form-label">Nome: </label>
+    <input type="text" name="nome" class="form-control" placeholder="Insira seu nome" value="">
   </div>
-  
+  <div class="mb-3">
+    <label for="mensagem" class="form-label">Deixe sua mensagem: </label>
+    <textarea name="mensagem" id="campoForum" class="form-control" placeholder="Proibido ofensas e expreções racistas" rows="5"></textarea>
+  </div>   
+    <input type="submit" name="enviar" value="Enviar" class="btn btn-primary" />
+</form>
+
+<div class="container-mensagem background-Lightgray box-shadow">
+  <div class="flex-mensagem-foto">
+    <img src="/img/perfilsemFoto.jpg" alt="Imagem_Perfil" class="img-perfil-forum">
+    <span class="nome-perfil-forum">Arthur Fernades</span>
+  </div>
+  <p class="texto-mensagem-forum">A semana da Ciência vai ser super legal!</p>
+</div>
+
+<div class="container-mensagem background-Lightgray box-shadow">
+  <div class="flex-mensagem-foto">
+    <img src="/img/perfilsemFoto.jpg" alt="Imagem_Perfil" class="img-perfil-forum">
+    <span class="nome-perfil-forum">Cláudio</span>
+  </div>
+  <p class="texto-mensagem-forum">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam repellendus tempora facere aliquam unde animi, officia praesentium ipsum pariatur recusandae eius ab accusamus ipsa voluptates mollitia soluta inventore eligendi nihil.!</p>
+</div>
+
 @endsection
